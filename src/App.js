6f60366422be6622c69def1c109410de6a25baf4
KhaleaM <106846809+KhaleaM@users.noreply.github.com>
2026-08-03 18:01:45 -0500

    added function to the 'search' bar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import logo from "./logo.png";
+import Dictionary from "./Dictionary";
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img
-          src={logo}
-          className="App-logo img-fluid"
-          alt="logo"
-        />
+        <img src={logo} className="App-logo img-fluid" alt="logo" />
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer text-center">
+        <small>Coded by Khalea Mitchell</small>
+      </footer>
     </div>
   );
 }
