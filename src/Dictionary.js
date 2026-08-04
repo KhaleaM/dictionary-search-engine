654429189a72c669891335461f548ea6b4010831
KhaleaM <106846809+KhaleaM@users.noreply.github.com>
2026-08-04 18:09:34 -0500

    fixed the dictionary

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -4,34 +4,31 @@ import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
-    let [keyword, setKeyword] = useState("");
-    let [results, setResults] = useState(null);
+  let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
+  function handleResponse(response) {
+    setResults(response.data[0]);
+  }
 
-    function handleResponse(response) {
-        setResults(response.data[0]);
-    }
+  function search(event) {
+    event.preventDefault();
 
-    function search(event) {
-      event.preventDefault();
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    console.log(apiUrl);
+    axios.get(apiUrl).then(handleResponse);
+  }
 
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
 
-      let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-      console.log(apiUrl);
-      axios.get(apiUrl).then(handleResponse);
-    }
-
-    function handleKeywordChange(event) {
-      setKeyword(event.target.value);
-    }
-
-
-    return (
-        <div className="Dictionary">
-            <form onSubmit={search}>
-                <input type="search" onChange={handleKeywordChange} autoFocus={true} />
-            </form>
-            <Results results={results} />
-        </div>
-    )
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" onChange={handleKeywordChange} autoFocus={true} />
+      </form>
+      <Results results={results} />
+    </div>
+  );
 }
