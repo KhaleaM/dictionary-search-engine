e8e2ad6c9b1e8c4bea279d3c4b6abb0caa0f837a
KhaleaM <106846809+KhaleaM@users.noreply.github.com>
2026-08-04 22:44:07 -0500

    fixed the 'images' API and styled

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,21 +1,32 @@
 import React, { useState } from "react";
 import Results from "./Results";
 import axios from "axios";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     setResults(response.data[0]);
+  }
+
+  function handleSheCodesResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     console.log(apiUrl);
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    let apiKey = "e2t5dd99b4cfa003ed9aaof63c44d73c";
+    let sheCodesApiUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${apiKey}&per_page=9`;
+    let headers = { Authorization: `Bearer ${apiKey}` };
+    axios.get(sheCodesApiUrl, { headers: headers }).then(handleSheCodesResponse);
   }
 
   function handleSubmit(event) {
@@ -49,6 +60,7 @@ export default function Dictionary(props) {
           </div>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
