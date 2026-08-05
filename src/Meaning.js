7c9e14e6cc3a019adb326a04e78532c31c9009e6
KhaleaM <106846809+KhaleaM@users.noreply.github.com>
2026-08-04 20:59:56 -0500

    fixed 'synonyms'

--- src/Meaning.js
+++ src/Meaning.js
@@ -10,13 +10,9 @@ export default function Meaning(props) {
         {props.meaning.definitions.map(function (definition, index) {
             return (
               <div key={index}>
-                <div className="definition">
-                    {definition.definition}
-                </div>
+                <div className="definition">{definition.definition}</div>
                 <div className="example">{definition.example}</div>
-
-                    <Synonyms synonyms={definition.synonyms} />
-                 
+                <Synonyms synonyms={definition.synonyms} />
               </div>
             );
         })}
