43078b8de22061264c588844300779888418e4fa
KhaleaM <106846809+KhaleaM@users.noreply.github.com>
2026-08-04 20:51:53 -0500

    styled the application

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,11 +1,17 @@
 import React from "react";
+import "./Phonetic.css";
 
 export default function Phonetic(props) {
     return (
-        <div className="Phonetic">
-            <a href={props.phonetic.audio} target="_blank" rel="noopener noreferrer">Listen</a>
-            <br />
-            {props.phonetic.text}
-        </div>
-    )
+      <div className="Phonetic">
+        <a
+          href={props.phonetic.audio}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Listen
+        </a>
+        <span className="text">{props.phonetic.text}</span>
+      </div>
+    );
 }
